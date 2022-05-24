02596dd5fcf956e57bfcc8432cafaa19b043e60e
yourcomrade <MinhXecole@gmail.com>
2022-05-24 18:57:01 +0200

    Code work for both following the line and avoiding obstacle

--- project_system_car.ino
+++ project_system_car.ino
@@ -8,21 +8,21 @@
 #define FORWARD 0
 #define STOP 1
 #define BACK 2
-#define my_speed 55
-#define an_stra 30//angle to turn straight is 30 degree
-#define an_left 70//angle to turn left is 60 degree
+#define my_speed 60
+#define an_stra 70//angle to turn straight is 30 degree
+#define an_left 140//angle to turn left is 60 degree
 #define an_rig 0//angle to turn right is 0 degree
 int prev_state = 0;
 void setup() {
   // put your setup code here, to run once:
   pin_mode();
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
   turn(an_stra);
-  uint8_t dist=0;//distance for the car
+}
+void avoid(){//function to control avoid obstacle
+// turn(an_stra);
+ uint8_t dist=0;//distance for the car
   if(has_obs(&dist)){
+    turn(an_stra);
     while(has_obs(&dist)){
       Serial.println("Move backward to start avoid obstacle");
      // motor(0,STOP);
@@ -34,46 +34,63 @@ void loop() {
     turn(an_rig);
     delay(200);
     Serial.println("Turn right");
-    motor(my_speed,FORWARD);
-    delay(1200);
+    motor(50,FORWARD);
+    delay(1600);
     motor(0,STOP);
     turn(an_left);
-    delay(500);
+    delay(200);
     Serial.println("Turn left");
     motor(60,FORWARD);
-    delay (2000);
     
+   // motor(30,FORWARD);
+    //motor(0,STOP);
+    //delay(10000);
+  }}void loop() {
+  // put your main code here, to run repeatedly:
+  
+ 
     //turn(an_stra);
     //motor(my_speed,FORWARD);
     //delay(3000);
-  }
-if(mid()){
+  if(data_r()&&data_l()){
+    Serial.println("Stop");
+  motor(0,STOP);
+  delay(5000);
+  
+}
+else if(mid()){
   Serial.println("Straight");
   turn(an_stra);
-  motor(47,FORWARD);
+  motor(35,FORWARD);
+  avoid();
 }
 else if(left()){
-  
-  int i=30;
-  do{if(i<70){i+=2;}
-  Serial.print("Left i= ");
-  Serial.println(i);
-  turn(i);
-  motor(my_speed,FORWARD);
+  int angle=an_stra;//angle for the car, the angle will increase as the left
+  int cspeed=30;
+  do{if(angle<140){angle+=5;}
+  Serial.print("Left angle= ");
+  Serial.println(angle);
+  turn(angle);
+  if(cspeed<55)cspeed;
+  motor(cspeed,FORWARD);
   delay(1);
-    }while(!mid()&&left());
+  //avoid();
+    }while(left());
 }
 else if(right()){
- 
-  int i=30;
-  do{if(i>=10){
-    i-=2;
+  int cspeed=30;
+  int angle=an_stra;
+  do{if(angle>=5){
+    angle-=5;
   }
-  Serial.print("Right i= ");
-  Serial.println(i);
-  turn(i);
-  motor(my_speed,FORWARD);
+  Serial.print("Right angle= ");
+  Serial.println(angle);
+  turn(angle);
+  if(cspeed<55)cspeed++;
+  motor(cspeed,FORWARD);
   delay(1);
-  }while(!mid()&&right());
+  //avoid();
+  }while(right());
 }
+
 }
